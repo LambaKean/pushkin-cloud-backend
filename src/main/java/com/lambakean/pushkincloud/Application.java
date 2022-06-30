3b30cbe04dc107684fbada0d4311ce86b012284d
LambaKean <lambakean@gmail.com>
2022-06-30 21:08:34 +0300

    Initial commit.

---
+++ src/main/java/com/lambakean/pushkincloud/Application.java
@@ -0,0 +1,12 @@
+package com.lambakean.pushkincloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
